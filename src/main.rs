812aeb251e223b001e1bc298eb4b351bc5f05a31
damezuma <qweads12@gmail.com>
2018-07-06 03:39:14 +0900

    ip와 port번호를 커멘드라인으로 받는 방법을 변경

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@ mod server;
 fn main() {
     let mut args = std::env::args();
     let mut port_number = None;
+    let mut ip:Option<Vec<u16>> = None;
     while let Some(arg) = args.next(){
+        if arg.len() > 2 && &arg.as_bytes()[0..2] == b"-p"{
+            port_number = Some( arg.as_str()[2..].parse::<u16>().unwrap());
+            continue;
+        }
+        if arg.len() >2 && &arg.as_bytes()[0..3] == b"-ip"{
+            ip = Some(arg.as_str()[3..].split(".").map(|it| it.parse::<u16>().unwrap()).collect());
+            continue;                
+        }
         match arg.as_str(){
-            "-p"|"--port"=>{
-                if let Some(v)= args.next(){
-                    port_number = Some(v.parse::<u16>().unwrap());
-                }
-            },
-            "-i"|"--ip"=>{
-
-            }
             "-c"|"--client"=>{
 
             },
